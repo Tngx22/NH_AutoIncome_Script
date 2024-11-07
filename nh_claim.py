1c8ef8b2109785e1d8a66ff1881463eaa42487ad
nhtngxaja <tngxpoolunik@gmail.com>
2024-11-08 03:22:31 +0700

    Update nh_claim.py

--- nh_claim.py
+++ nh_claim.py
@@ -45,10 +45,10 @@ CONFIG_DETAIL = {
     }
 }
 
-BROWSER = 'Firefox'
+BROWSER = 'Chrome'
 PATH = Path(__file__).parent
 LOGIN_URL = 'http://kageherostudio.com/event/?event=daily#'
-CLAIM_URL = 'https://www.kageherostudio.com/event/?event=daily'
+CLAIM_URL = 'http://kageherostudio.com/event/?event=daily'
 config = CONFIG_DETAIL[BROWSER.lower()]
 
 
@@ -173,8 +173,7 @@ if __name__ == '__main__':
 
     print('\n'.join(f'{str(i+1)+".":<3} {j.get("username")}' for i, j in enumerate(data)))
 
-    num = input(f'Starting Point(1 - {len(data)}):9')
+    num = input(f'Starting Point(1 - {len(data)}): ')
     print()
 
     main(data, min(len(data), max(1, int(num)))-1)
-677
